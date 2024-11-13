76b65d1b2e7a9992a1a6f0e7e5cb1a5124d70e47
rbnlngs <langiusruben@gmail.com>
2024-11-13 22:40:46 +0100

    add custom root ID and version numbers to enable clientside injecting.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { version } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,16 @@ export default defineConfig({
       scss: {
         api: "modern-compiler",
         quietDeps: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // Format the entry file names and chunk file names with the version
+        entryFileNames: `[name]-v${version}.js`,
+        chunkFileNames: `[name]-v${version}.js`,
+        assetFileNames: `[name]-v${version}.[ext]`,
       },
     },
   },
