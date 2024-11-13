da9d2c74ad48d1cd935c2dd699be0a1c73ed2a94
rbnlngs <langiusruben@gmail.com>
2024-11-13 18:53:32 +0100

    add auto detect for languages

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,20 +2,33 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import enTranslation from "./en/translation.json";
 import nlTranslation from "./nl/translation.json";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-i18next.use(initReactI18next).init({
-  lng: "en", // if you're using a language detector, do not define the lng option
-  fallbackLng: "en",
-  debug: true,
-  resources: {
-    en: {
-      translation: enTranslation,
+i18next
+  .use(LanguageDetector) // Add the language detector
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en", // Default language if detection fails
+    debug: true,
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      nl: {
+        translation: nlTranslation,
+      },
     },
-    nl: {
-      translation: nlTranslation,
+    detection: {
+      // Prioritize localStorage over browser language
+      order: [
+        "localStorage",
+        "querystring",
+        "htmlTag",
+        "navigator",
+        "path",
+        "subdomain",
+      ],
+      caches: ["localStorage"],
     },
-  },
-  // if you see an error like: "Argument of type 'DefaultTFuncReturn' is not assignable to parameter of type xyz"
-  // set returnNull to false (and also in the i18next.d.ts options)
-  returnNull: false,
-});
+    returnNull: false,
+  });
