505694ff13f375c14efd251066889578c2ab5566
Veiko Aunapuu <veiko.aunapuu@gmail.com>
2025-07-07 13:02:25 +0300

    Remove dlploy and update viet.config.js for GithubPages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { version } from "./package.json";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AI-kusimused/',
   css: {
     preprocessorOptions: {
       scss: {
