e23c5191533eac19ba52bd05c6efd658f6f5fb40
vaunapuu <veiko.aunapuu@gmail.com>
2025-11-01 04:19:30 +0200

    Intro teksti muudatused

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -26,41 +26,10 @@ export default function ({
           </h3>
           <br />
           <p>
-            {/* first sentence */}
-            <a
-              href="https://eur-lex.europa.eu/eli/reg/2024/1689/oj"
-              target="_blank"
-            >
-              {t("AI_Act_link")}
-            </a>
-            {/* second sentence */}
-            {t("description2")}
-            <ul>
-              <li>{t("prohibited")}</li>
-              <li>{t("high_risk")}</li>
-              <li>{t("limited_risk")}</li>
-              <li>{t("general_purpose")}</li>
-              <li>{t("minimal_risk")}</li>
-            </ul>
-            {t("description3")}
-            {/* third sentence */}
-          </p>
-          <p>
             <h5>
               <b>{t("bold1")}</b>
             </h5>
             {t("description4")}
-          </p>
-          <p>
-            {t("description5")}
-            <b>{t("bold2")}</b>
-            {t("description6")}
-            <ol>
-              <li>{t("interactive_pages")}</li>
-              <li>{t("vestlusrobot")}</li>
-              <li>{t("algoritmiregister")}</li>
-              <li>{t("riskihinnangute_register")}</li>
-            </ol>
           </p>
           <p>
             <h5>
@@ -68,41 +37,7 @@ export default function ({
             </h5>
             {t("description7")}
           </p>
-          <p>
-            {t("description8")}
-            <b>{t("bold4")}</b>
-            {t("description9")}
-          </p>
-          <h5>
-            <b>{t("Tagasiside")}</b>
-          </h5>
-          <p>{t("description10")}</p>
-          <p>
-            {t("See")} <b>{t("bold5")}</b> {t("tagasiside_info")}
-            <b>
-              <a
-                href="https://docs.google.com/forms/d/e/1FAIpQLSfsc-VX64veFhhEeNZs6XrmZ9d03dJndSlBnow3Jv1zsySsRw/viewform?usp=header"
-                target="_blank"
-              >
-                {t("kusimustiku_link")}
-              </a>
-            </b>
-            {t("description11")}
-          </p>
-          <h5>
-            <b>{t("bold6")}</b>
-          </h5>
-          <p>{t("description12")}</p>
-          <p>{t("description13")}</p>
-          <p>
-            <b>
-              <i>{t("bold7")}</i>
-            </b>
-            {t("description14")}
-            <i>{t("cursive1")}</i>
-            {t("description15")}
-          </p>
-          <p style={{ color: "red" }}>{t("description16")}</p>
+          <p>{t("description8")}</p>
           <p>
             <i>
               {t("feedback_0")}
